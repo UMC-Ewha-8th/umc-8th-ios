58906e0b272077abfeef33c800075c0ee6c784c9
rhy2301 <gangri20040512@gmail.com>
2025-04-10 01:28:00 +0900

    Delete UMC_Starbucks  directory

--- UMC_Starbucks /UMC_Starbucks/Models/LoginModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LoginModel.swift
-//  UMC_Starbucks
-//
-//  Created by 강리현 on 3/24/25.
-//
-
-import Foundation
--- UMC_Starbucks /UMC_Starbucks/ViewModels/OtherViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  OtherViewModel.swift
-//  UMC_Starbucks
-//
-//  Created by 강리현 on 3/29/25.
-//
-
-import Foundation
